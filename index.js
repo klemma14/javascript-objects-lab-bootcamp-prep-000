4d4b7b03968954f6f115423c1b18fdd575b423ad
Kalkidan Lemma <k.lemma14@gmail.com>
2019-03-08 17:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object,key,value) {
   return newRecipes;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  var recipes.prop2 = '2';
+ recipes.prop2 = '2';
   return recipes;
 }
 function deleteFromObjectByKey(object,key) {
