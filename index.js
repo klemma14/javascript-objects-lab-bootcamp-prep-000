fa9fa9aeff2309a5d78f968bec017d17afa78334
Kalkidan Lemma <k.lemma14@gmail.com>
2019-03-08 17:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var recipes = {prop: 1}
+var recipes = {}
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
  recipes.prop2 = '2';
   return recipes;
 }
-
+  const recipes = {prop: 1}
+  
 function updateObjectWithKeyAndValue(object,key,value) {
   var newRecipes = Object.assign({},recipes,{prop: 1},{prop2: 2});
   return newRecipes;
